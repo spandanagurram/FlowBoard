68caef678b5774e60871ac3e9b681cfe98ef9fc5
Gurram-Spandana <spandana.vrl@gmail.com>
2026-06-30 19:19:26 +0530

    Added refresh token

--- backend/apps/accounts/urls.py
+++ backend/apps/accounts/urls.py
@@ -1,5 +1,5 @@
 from django.urls import path
-
+from rest_framework_simplejwt.views import TokenRefreshView
 from .views import LoginAPIView, ProfileAPIView, RegisterAPIView
 
 
@@ -7,4 +7,5 @@ urlpatterns = [
     path('register/', RegisterAPIView.as_view(), name='register'),
     path('login/', LoginAPIView.as_view(), name='login'),
     path('profile/', ProfileAPIView.as_view(), name='profile'),
+    path('token/refresh/', TokenRefreshView.as_view(), name='token_refresh'),
 ]
